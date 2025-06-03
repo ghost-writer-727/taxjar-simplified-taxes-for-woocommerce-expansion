51156a24ab9a7301857d5192361396fed58e9b07
Ghost Writer <4065416+ghost-writer-727@users.noreply.github.com>
2025-06-03 17:11:22 +0000

    Set up the patch 2.2.1

---
+++ db-patch-2-2-1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TaxJarExpansion;
+defined( 'ABSPATH' ) || exit;
+
+add_action('admin_init', function() {
+    // Check if the current page is wp-admin/admin.php?page=wc-settings&tab=taxjar-integration&patch=2-2-1
+    if( ! isset($_GET['page']) || $_GET['page'] !== 'wc-settings' || ! isset($_GET['tab']) || $_GET['tab'] !== 'taxjar-integration' || ! isset($_GET['patch']) || $_GET['patch'] !== '2-2-1'){
+        return;
+    }
+    UserProfile::get_instance()->patch_2_2_1();
+});
